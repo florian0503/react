3ed5bd4ec8125ffb1a00ebbc4f6581b0f49c1f5f
florian0503 <florian.dimbert@next-u.fr>
2024-06-09 23:57:20 +0200

    version final

--- react/src/components/Pokem.jsx
+++ react/src/components/Pokem.jsx
@@ -24,23 +24,23 @@ const Pokem = () => {
   }
 
   return (
-    <div>
-      <h2>{pokemon.name}</h2>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-      <h3>Types:</h3>
-      <ul>
+    <div className="Bloup2">
+      <h2 className="Bloup3">{pokemon.name}</h2>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} className="Bloup4"/>
+      <h3 className="Bloup">Types:</h3>
+      <ul className="Bloup5">
         {pokemon.types.map((type, index) => (
           <li key={index}>{type.type.name}</li>
         ))}
       </ul>
-      <h3>Abilities:</h3>
-      <ul>
+      <h3 className="Bloup">Abilities:</h3>
+      <ul className="Bloup5">
         {pokemon.abilities.map((ability, index) => (
           <li key={index}>{ability.ability.name}</li>
         ))}
       </ul>
-      <h3>Stats:</h3>
-      <ul>
+      <h3 className="Bloup">Stats:</h3>
+      <ul className="Bloup5">
         {pokemon.stats.map((stat, index) => (
           <li key={index}>{stat.stat.name}: {stat.base_stat}</li>
         ))}
